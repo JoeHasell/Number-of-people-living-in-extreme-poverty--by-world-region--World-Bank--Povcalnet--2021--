336465c70474052993d80439cbbc5d97f34f5c5c
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2021-11-19 15:50:10 +0000

    Changes to test script

--- Test 1.py
+++ Test 1.py
@@ -1,7 +1,11 @@
 
 import pandas as pd
+msg = "Hi Joe"
+print(msg)
 
-df = pd.read_csv (r'Data/Poverty and inequality measures from PovCal (2021).csv')
+df = pd.read_csv('Data/Poverty and inequality measures from PovCal (2021).csv')
+
+df.describe()
 
 df.head()
 
